f21f269f591db1042e7a50d33ab2ee6d406cca47
piero <piero_chen@163.com>
2015-12-20 19:41:18 +0800

    add WecharUser for  Message

--- DoShineMP/Models/Message.cs
+++ DoShineMP/Models/Message.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,10 @@ namespace DoShineMP.Models
 
         public string Content { get; set; }
 
-        public int?
+        public int? WechatUserId { get; set; }
+
+        [ForeignKey("WechatUserId")]
+        public virtual WechatUser WechatUser { get; set; }
 
         public MessageType Type { get; set; }
 
