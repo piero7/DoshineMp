0099e5f7c77e6d4fc424d77e40e6d93280bf6af8
piero <piero_chen@163.com>
2015-12-25 14:59:14 +0800

    add send code function

---
+++ DoShineMP/Controllers/ShortMessageController.cs
@@ -0,0 +1,55 @@
+﻿using DoShineMP.Helper;
+using DoShineMP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DoShineMP.Controllers
+{
+    public class ShortMessageController : ApiController
+    {
+        /// <summary>
+        /// 发送验证码，若成功则返回ID后续一并提交，若未成功则返回0
+        /// </summary>
+        /// <param name="openid">openid</param>
+        /// <param name="number">电话号码</param>
+        /// <returns></returns>
+        [HttpGet]
+        public int GetIndentifyingCode(string openid, string number)
+        {
+            var code = (DateTime.Now.ToString() + openid).GetHashCode().ToString().Replace("-", "");
+            code = code.PadLeft(6, '0').Substring(code.Length - 6, 6);
+
+            var ic = new IdentifyingCode
+            {
+                OpenId = openid,
+                Content = code,
+                PhoneNumber = number,
+            };
+
+            var codeStirng = string.Format(System.Configuration.ConfigurationManager.AppSettings["identifyingcodebasestring"], code);
+            var ret = ShortMessageHelper.SendMsg(number, codeStirng);
+
+            var db = new ModelContext();
+
+            if (ret == "s")
+            {
+                ic.IsSendSuccess = true;
+                db.IdentifyingCodeSet.Add(ic);
+                db.SaveChanges();
+                return ic.IdentifyingCodeId;
+            }
+            else
+            {
+                ic.IsSendSuccess = false;
+                ic.Remarks = ret;
+                db.IdentifyingCodeSet.Add(ic);
+                db.SaveChanges();
+                return 0;
+            }
+        }
+    }
+}
--- DoShineMP/Models/IdentifyingCode.cs
+++ DoShineMP/Models/IdentifyingCode.cs
@@ -18,6 +18,19 @@ namespace DoShineMP.Models
 
         public string Content { get; set; }
 
+        public string PhoneNumber { get; set; }
+
+        public bool IsSendSuccess { get; set; }
+
+        public bool IsUsed { get; set; }
+
         public string Remarks { get; set; }
+
+        public IdentifyingCode()
+        {
+            this.CreateDate = DateTime.Now;
+            this.IsUsed = false;
+            this.IsSendSuccess = false;
+        }
     }
 }
