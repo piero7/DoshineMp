1f51d7fd8e8330e632524542956d47c60b8f5f21
piero <piero_chen@163.com>
2016-01-11 15:58:44 +0800

    add village (not finish)

---
+++ DoShineMP/Models/Village.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoShineMP.Models
+{
+    /// <summary>
+    /// 小区表
+    /// </summary>
+    public class Village
+    {
+        [Key]
+        public int VillageId { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
